2d0d72951b3f686d01cf849194295cec797715ca
Cailey Cline <cicline@mail.lipscomb.edu>
2018-04-16 02:23:29 -0500

    Cleaned comments

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,40 +1,27 @@
-/*-----( Import needed libraries )-----*/
 #include <Stepper.h>
 
-/*-----( Declare Constants, Pin Numbers )-----*/
 //---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
-#define STEPS_PER_MOTOR_REVOLUTION 32   
+#define STEPS_PER_MOTOR_REVOLUTION 32
 
 //---( Steps per OUTPUT SHAFT of gear reduction )---
-#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
+#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048
 
-/*-----( Declare objects )-----*/
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
 // attached to the pin connections need to be pins 8,9,10,11 connected
-// to Motor Driver In1, In2, In3, In4 
+// to Motor Driver In1, In2, In3, In4
 
 // Then the pins are entered here in the sequence 1-3-2-4 for proper sequencing
 Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
 
-
-/*-----( Declare Variables )-----*/
 int  Steps2Take;
 
-void setup()   /*----( SETUP: RUNS ONCE )----*/
-{
-// Nothing  (Stepper Library sets pins as outputs)
-}/*--(end setup )---*/
+void setup(){
+  //(Stepper Library sets pins as outputs)
+}
 
-void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
-{
-  small_stepper.setSpeed(1000);   // SLOWLY Show the 4 step sequence 
+void loop(){
+  small_stepper.setSpeed(1000);   // SLOWLY Show the 4 step sequence
   Steps2Take  =  5.125*STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CW
   small_stepper.step(Steps2Take);
   delay(10000);
-
-
-
-}/* --(end main loop )-- */
-
-/* ( THE END ) */
