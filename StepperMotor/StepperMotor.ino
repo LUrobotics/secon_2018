5372678b6731155e640b55b2826ac531513465cb
Reid-Weber <31290249+Reid-Weber@users.noreply.github.com>
2018-04-17 20:45:18 -0500

    Adding interrupt capabilities to captains wheel code

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,27 +1,46 @@
+/*-----( Import needed libraries )-----*/
 #include <Stepper.h>
 
+/*-----( Declare Constants, Pin Numbers )-----*/
 //---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
-#define STEPS_PER_MOTOR_REVOLUTION 32
+#define STEPS_PER_MOTOR_REVOLUTION 32   
 
 //---( Steps per OUTPUT SHAFT of gear reduction )---
-#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048
+#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
 
+#define INTERRUPT 4
+
+/*-----( Declare objects )-----*/
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
-// attached to the pin connections need to be pins 8,9,10,11 connected
-// to Motor Driver In1, In2, In3, In4
+// attached to the pin connections need to be pins 0,1,2,3 connected
+// to Motor Driver In1, In2, In3, In4 
 
 // Then the pins are entered here in the sequence 1-3-2-4 for proper sequencing
-Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
+Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 0, 2, 1, 3);
 
+bool hasWheelTurned = false;
+
+
+/*-----( Declare Variables )-----*/
 int  Steps2Take;
 
-void setup(){
-  //(Stepper Library sets pins as outputs)
-}
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+// Nothing  (Stepper Library sets pins as outputs)
+  pinMode(INTERRUPT, INPUT);
+}/*--(end setup )---*/
 
-void loop(){
-  small_stepper.setSpeed(1000);   // SLOWLY Show the 4 step sequence
-  Steps2Take  =  5.125*STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CW
-  small_stepper.step(Steps2Take);
-  delay(10000);
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if(digitalRead(INTERRUPT) == false && hasWheelTurned == true){
+    small_stepper.setSpeed(1000);   // SLOWLY Show the 4 step sequence 
+    Steps2Take  =  5.125*STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CW
+    small_stepper.step(Steps2Take);
+  }
+
+  delay(10);
+  
+}/* --(end main loop )-- */
+
+/* ( THE END ) */
