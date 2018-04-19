1b3d874f7a0350603f3092346a71e49aecd92032
Cailey Cline <cicline@mail.lipscomb.edu>
2018-04-19 00:09:39 -0400

    Generalized test procedure

--- System Testing/LIDAR_testing/LIDAR_testing.ino
+++ System Testing/LIDAR_testing/LIDAR_testing.ino
@@ -1,6 +1,12 @@
 #include "Adafruit_VL53L0X.h" // https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/arduino-code
 #include "Adafruit_VL6180X.h" // https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/wiring-and-test
 #include <Wire.h>
+#include <IRLibDecodeBase.h> 
+#include <IRLib_HashRaw.h>  //Must be last protocol
+#include <IRLibCombo.h>     // After all protocols, include this
+#include <IRLibRecv.h> 
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
 #define TCAADDR1 0x71
 #define TCAADDR2 0x72
@@ -8,69 +14,76 @@
 
 Adafruit_VL6180X shortRange = Adafruit_VL6180X();
 Adafruit_VL53L0X longRange = Adafruit_VL53L0X();
+Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+  Wire.begin(); // join i2c bus as master
   Serial.begin(9600);
   while(!Serial);
-  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("Hello!");
-  
-  Wire.begin(); // join i2c bus as master
+  matrix.begin(0x70);
+  // blink colon on 7-segment display until signal received
+  matrix.blinkRate(2);
+  matrix.drawColon(true);
+  matrix.writeDisplay();
+
+  delay(5000);/
   I2CSelect(1, -1);
   I2CSelect(2, -1);
   I2CSelect(3, -1);
 
   Serial.println("BOARD 1");
   I2CSelect(1, 0);
-  shortRange.begin();
-  Serial.println("0");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("0");
   I2CSelect(1, 1);
-  shortRange.begin();
-  Serial.println("1");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("1");
   I2CSelect(1, 2);
-  longRange.begin();
-  Serial.println("2");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("2");
   I2CSelect(1, 3);
-  longRange.begin();
-  Serial.println("3");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("3");
   I2CSelect(1, 4);
-  longRange.begin();
-  Serial.println("4");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("4");
   I2CSelect(1, 5);
-  longRange.begin();
-  Serial.println("5");
+  Serial.println("Trying...");
+  if(longRange.begin()) Serial.println("5");
   I2CSelect(1, -1);
+  Serial.println("\n Setup Complete");
 
-  Serial.println("BOARD 2");
-  I2CSelect(2, 0);
-  longRange.begin();
-  Serial.println("0");
-  I2CSelect(2, 1);
-  shortRange.begin();
-  Serial.println("1");
-  I2CSelect(2, 2);
-  shortRange.begin();
-  Serial.println("2");
-  I2CSelect(2, 3);
-  shortRange.begin();
-  Serial.println("3");
-  I2CSelect(2, 4);
-  longRange.begin();
-  Serial.println("4");
-  I2CSelect(2, 5);
-  longRange.begin();
-  Serial.println("5");
-  I2CSelect(2, -1);
+//  Serial.println("BOARD 2");
+//  I2CSelect(2, 0);
+//  longRange.begin();
+//  Serial.println("0");
+//  I2CSelect(2, 1);
+//  shortRange.begin();
+//  Serial.println("1");
+//  I2CSelect(2, 2);
+//  shortRange.begin();
+//  Serial.println("2");
+//  I2CSelect(2, 3);
+//  shortRange.begin();
+//  Serial.println("3");
+//  I2CSelect(2, 4);
+//  longRange.begin();
+//  Serial.println("4");
+//  I2CSelect(2, 5);
+//  longRange.begin();
+//  Serial.println("5");
+//  I2CSelect(2, -1);
 
-  Serial.println("BOARD 3");
-  I2CSelect(3, 0);
-  longRange.begin();
-  Serial.println("0");
-  I2CSelect(3, 1);
-  longRange.begin();
-  Serial.println("1");
+//  Serial.println("BOARD 3");
+//  I2CSelect(3, 0);
+//  longRange.begin();
+//  Serial.println("0");
+//  I2CSelect(3, 1);
+//  longRange.begin();
+//  Serial.println("1");
 //  I2CSelect(3, 2);
 //  shortRange.begin();
 //  Serial.println("2");
@@ -83,89 +96,13 @@ void setup() {
 //  I2CSelect(3, 5);
 //  shortRange.begin();
 //  Serial.println("5");
-  I2CSelect(3, -1);
+//  I2CSelect(3, -1);
 
 }
 
 void loop() {
-  Serial.println("BOARD 1");
-  Serial.println("\nSensor 0");
-  I2CSelect(1,0);
-  ReadShort();
-  delay(1000);
-
-  Serial.println("\nSensor 1");
-  I2CSelect(1,1);
-  ReadShort();
-  delay(1000);
-
-  Serial.println("\nSensor 2");
-  I2CSelect(1,2);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 3");
-  I2CSelect(1,3);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 4");
-  I2CSelect(1,4);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 5");
-  I2CSelect(1,5);
-  ReadLong();
-  I2CSelect(1,-1);
-  delay(2000); 
-
-  
-
-  Serial.println("\n\nBOARD 2");
-  Serial.println("\nSensor 0");
-  I2CSelect(2,0);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 1");
-  I2CSelect(2,1);
-  ReadShort();
-  delay(1000);
-
-//  Serial.println("\nSensor 2");
-//  I2CSelect(2,2);
-//  ReadShort();
-//  delay(1000);
-
-  Serial.println("\nSensor 3");
-  I2CSelect(2,3);
-  ReadShort();
-  delay(1000);
-
-  Serial.println("\nSensor 4");
-  I2CSelect(2,4);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 5");
-  I2CSelect(2,5);
-  ReadLong();
-  I2CSelect(2,-1);
-  delay(2000); 
-
-  Serial.println("\n\nBOARD 3");
-  Serial.println("\nSensor 0");
-  I2CSelect(3,0);
-  ReadLong();
-  delay(1000);
-
-  Serial.println("\nSensor 1");
-  I2CSelect(3,1);
-  ReadLong();
-  I2CSelect(3,-1);
-  delay(2000);
-
+  testMux(1);
+  delay(5000);
 }
 
 void I2CSelect(int mux, int8_t i) {
@@ -195,7 +132,7 @@ void I2CSelect(int mux, int8_t i) {
 }
 
 float ReadShort(){
-  float lux = shortRange.readLux(VL6180X_ALS_GAIN_5);
+//  float lux = shortRange.readLux(VL6180X_ALS_GAIN_5);
   uint8_t range = shortRange.readRange();
   uint8_t status = shortRange.readRangeStatus();
   if (status == VL6180X_ERROR_NONE) {
@@ -232,15 +169,52 @@ float ReadShort(){
   return range;
 }
 
-float ReadLong(){
-  VL53L0X_RangingMeasurementData_t measure;
-  longRange.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+//float ReadLong(){
+//  VL53L0X_RangingMeasurementData_t measure;
+//  longRange.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+//
+//  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+//    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+//  } else {
+//    Serial.println(" out of range ");
+//  }
+//  return measure.RangeMilliMeter;
+//}
 
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-  } else {
-    Serial.println(" out of range ");
-  }
-  return measure.RangeMilliMeter;
+void testMux(int muxID){
+  int testTime = 50;
+  
+  Serial.println("----------------");
+  Serial.print("BOARD ");
+  Serial.println(muxID);
+  Serial.println("\nSensor 0");
+  I2CSelect(muxID,0);
+  ReadShort();
+  delay(testTime);
+
+  Serial.println("\nSensor 1");
+  I2CSelect(muxID,1);
+  ReadShort();
+  delay(testTime);
+
+  Serial.println("\nSensor 2");
+  I2CSelect(muxID,2);
+  ReadShort();
+  delay(testTime);
+
+  Serial.println("\nSensor 3");
+  I2CSelect(muxID,3);
+  ReadShort();
+  delay(testTime);
+
+  Serial.println("\nSensor 4");
+  I2CSelect(muxID,4);
+  ReadShort();
+  delay(testTime);
+
+  Serial.println("\nSensor 5");
+  I2CSelect(muxID,5);
+  ReadShort();
+  I2CSelect(muxID,-1);
 }
 
