7660d530a210189575696d0e30a28d7bcb982dc7
Reid-Weber <31290249+Reid-Weber@users.noreply.github.com>
2018-04-19 23:00:27 -0500

    Adding IO test for trinket and feather

---
+++ IOTestFeather/IOTestFeather.ino
@@ -0,0 +1,23 @@
+#define IOPIN 5
+//bool hasSentMessage = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IOPIN, OUTPUT);
+  digitalWrite(IOPIN, HIGH);
+  delay(5000);
+  digitalWrite(IOPIN, LOW);
+  pinMode(IOPIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  if(hasSentMessage == false){
+    if(digitalRead(IOPIN) == LOW){
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+  }
+
+  delay(10);
+  
+}
---
+++ InputOutputTestTrinket/InputOutputTestTrinket.ino
@@ -0,0 +1,23 @@
+#define IOPIN 0
+bool hasSentMessage = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IOPIN, INPUT);
+  delay(1000);
+  while(digitalRead(IOPIN) != LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(hasSentMessage == false){
+    delay(2000);
+    pinMode(IOPIN, OUTPUT);
+    digitalWrite(IOPIN, LOW);
+    hasSentMessage = true;
+  }
+
+  delay(10);
+  
+  
+}
